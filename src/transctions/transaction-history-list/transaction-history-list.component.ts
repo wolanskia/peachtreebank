3eeb42515ff2593abb60c01c5ca07b721b03f3ea
Alex Wolanski <alexandria.wolanski@gmail.com>
2020-10-24 12:44:35 -0400

    feat(transactions): create transaction history list component

---
+++ src/transctions/transaction-history-list/transaction-history-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-history-list',
+  styleUrls: ['./transaction-history-list.component.scss'],
+  templateUrl: './transaction-history-list.component.html'
+})
+export class TransactionHistoryListComponent { }
--- src/transctions/transactions.module.ts
+++ src/transctions/transactions.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TransactionHistoryRecordComponent } from './transaction-history-record/transaction-history-record.component';
+import { TransactionHistoryListComponent } from './transaction-history-list/transaction-history-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TransactionHistoryRecordComponent],
-  exports: [TransactionHistoryRecordComponent]
+  declarations: [
+    TransactionHistoryListComponent,
+    TransactionHistoryRecordComponent
+  ],
+  exports: [
+    TransactionHistoryListComponent,
+    TransactionHistoryRecordComponent
+  ]
 })
 export class TransactionsModule { }
