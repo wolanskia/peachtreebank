e4497c1a46922e09803882a82a0a630c3bcf0b9d
Alex Wolanski <alexandria.wolanski@gmail.com>
2020-10-26 20:38:17 -0400

    fix(transer): subtract transfer amount from account balance

--- src/transfer/transfer.component.ts
+++ src/transfer/transfer.component.ts
@@ -3,6 +3,8 @@ import { transferForm } from './transfer.form';
 import { TransactionService } from 'src/common/services';
 import { FormGroup } from '@angular/forms';
 
+import { Transfer } from 'src/common/models';
+
 @Component({
   selector: 'app-transfer',
   styleUrls: ['./transfer.component.scss'],
@@ -10,10 +12,13 @@ import { FormGroup } from '@angular/forms';
 })
 export class TransferComponent {
   form: FormGroup = transferForm;
+  accountBalance = 5824.76;
 
   constructor(private transactionService: TransactionService) {}
 
-  createTransfer() {
-    this.transactionService.createTransaction(this.form.getRawValue());
+  createTransfer(transfer: Transfer) {
+    this.transactionService.createTransaction(transfer);
+    this.accountBalance = parseFloat(`${this.accountBalance - transfer.amount}`);
+    this.form.reset();
   }
 }
--- src/transfer/transfer.form.ts
+++ src/transfer/transfer.form.ts
@@ -3,5 +3,5 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export const transferForm = new FormGroup({
   fromAccount: new FormControl(''),
   toAccount: new FormControl('', Validators.required),
-  amount: new FormControl('', Validators.required)
+  amount: new FormControl('', [Validators.required, Validators.pattern(/^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/)])
 });
